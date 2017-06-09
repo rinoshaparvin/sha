fca568ef6588572baa930373da310d2b94f17b16
rinoshaparvin <rinosha14@gmail.com>
2017-06-09 18:33:34 +0530

    Create red.java

---
+++ red.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class red
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String y=new.scan();
+for(i=0;i<5;i++)
+{
+System.out.println("HELLO");
+}
+}
+
+}
